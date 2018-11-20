dc2193077f810f8272d93dcdecc7b12ba4e462af
BrandonPessman <pessman.brandon@gmail.com>
2018-11-19 20:14:47 -0600

    Add bootstrap

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+app.use("",express.static(__dirname + ""));
+
 router.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 router.get('/linkupload', (req, res) => {
-	res.sendFile(__dirname + '/linkupload.html');
+	res.sendFile(__dirname + '/front/linkupload.html');
+});
+
+router.get('/about', (req, res) => {
+	res.sendFile(__dirname + '/front/about.html');
+});
+
+router.get('/blog', (req, res) => {
+	res.sendFile(__dirname + '/front/blog.html');
+});
+
+router.get('/projects', (req, res) => {
+	res.sendFile(__dirname + '/front/projects.html');
+});
+
+router.get('/resume', (req, res) => {
+	res.sendFile(__dirname + '/front/resume.html');
 });
 
 app.use("/",router);
