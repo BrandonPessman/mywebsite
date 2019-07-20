c4df33112c0bd8aeb96bd629fa72a1f183eee108
BrandonPessman <pessman.brandon@gmail.com>
2019-07-19 22:35:49 -0500

    Minor changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Jumbotron, Col, Row, Container } from 'reactstrap'
 import Picture from '../images/profilepic.jpg'
-import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from 'mdbreact'
+import {
+  MDBMask,
+  MDBView,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  Animation,
+} from 'mdbreact'
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <center>
-          <img
-            src={Picture}
-            className="img-fluid rounded-circle mypicture"
-            alt=""
-            width="300"
-          />
+          <Animation type="jello" infinite duration="2s">
+            <img
+              src={Picture}
+              className="img-fluid rounded-circle mypicture"
+              alt=""
+              width="300"
+            />
+          </Animation>
         </center>
       </div>
     )
