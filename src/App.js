3072f04d92751e74eeb721bddf96a7a4b2a75ef3
brandonpessman <pessman.brandon@gmail.com>
2019-11-19 23:37:47 -0600

    Restart of page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import './App.css'
-import NavBar from './components/3-organisms/NavBar'
-
-// import Clearwater from './images/clearwater.jpeg'
-// import Innovation from './images/innovation.png'
-// import Travelers from './images/travelers.jpeg'
 
 function App () {
   return (
     <div>
-      <NavBar />
+      <h1>Hello World</h1>
     </div>
   )
 }
