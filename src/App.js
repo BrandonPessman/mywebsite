6d63b34e319c8d7a55e3ba5af3950a53fa9976f3
BrandonPessman <pessman.brandon@gmail.com>
2019-11-28 03:35:19 -0600

    Updates pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
 
 import Home from './components/Pages/Home'
 import Post from './components/Pages/Post'
@@ -12,15 +11,32 @@ import Post from './components/Pages/Post'
 function App () {
   return (
     <div>
+      <Navbar collapseOnSelect expand='lg' bg='light' variant='light'>
+        <Navbar.Brand href='/'>Brandon Pessman</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+            {/* <Nav.Link href='/'>Blog</Nav.Link> */}
+            {/* <NavDropdown title='Projects' id='collasible-nav-dropdown'>
+              <NavDropdown.Item href='/'></NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          <Nav>
+            <Nav.Link href='/'>New Post</Nav.Link>
+            <Nav.Link href='/'>Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
       <Router>
-      <Switch>
-        <Route path="/post/:id">
-          <Post />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path='/post/:id'>
+            <Post />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </div>
   )
--- src/components/Pages/Post.js
+++ src/components/Pages/Post.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 
 export default function Post () {
   const [author, setAuthor] = useState('')
@@ -10,35 +12,43 @@ export default function Post () {
   const [date, setDate] = useState('')
 
   let { id } = useParams()
-  axios
-    .get('http://localhost:8081/posts/' + id)
-    .then(function (response) {
-      // handle success
-      let data = response.data[0]
-      setAuthor(data.author)
-      setTitle(data.title)
-      let bodyHTML = new DOMParser().parseFromString(data.body, 'text/xml')
 
-      console.log(bodyHTML)
-      setBody(data.body)
-      setPostId(data._id)
-      setDate(data.date)
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error)
-    })
-    .finally(function () {
-      // always executed
-    })
+  useEffect(
+    () => {
+      axios
+        .get('http://localhost:8081/posts/' + id)
+        .then(function (response) {
+          // handle success
+          let data = response.data[0]
+          setAuthor(data.author)
+          setTitle(data.title)
+          setBody(data.body)
+          setPostId(data._id)
+          var ts = new Date(data.date)
+          setDate(ts.toDateString())
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error)
+        })
+        .finally(function () {
+          // always executed
+        })
+    },
+    [id]
+  )
 
   return (
     <div>
-      <h1>{title}</h1>
-      <h3>
-        {author} {date} {postId}
-      </h3>
-      <p>{body}</p>
+      <Jumbotron>
+        <center>
+          <h1>{title.toUpperCase()}</h1>
+          <h5>{date}</h5>
+        </center>
+      </Jumbotron>
+      <Container>
+        <h5>{body}</h5>
+      </Container>
     </div>
   )
 }
