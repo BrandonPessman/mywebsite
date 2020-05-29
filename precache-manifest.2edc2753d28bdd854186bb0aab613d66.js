fda8faf04df198c2df8e118e6789c7245cb47dca
Brandon Pessman <PESSMABM5831@uwec.edu>
2020-05-29 15:48:51 -0700

    Updates

---
+++ precache-manifest.2edc2753d28bdd854186bb0aab613d66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7726d9efae35f50e0bc92106d45a1021",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e596eba9128ab92f95",
+    "url": "/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "fcd6d8983e5ccf092b3a",
+    "url": "/static/css/main.024fe8ba.chunk.css"
+  },
+  {
+    "revision": "33e596eba9128ab92f95",
+    "url": "/static/js/2.9dca374a.chunk.js"
+  },
+  {
+    "revision": "2e0b94b74d604a77ce8bd28c426e1291",
+    "url": "/static/js/2.9dca374a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd6d8983e5ccf092b3a",
+    "url": "/static/js/main.a141ef31.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "24c552c9c50d011d2ce5f904644fffb6",
+    "url": "/static/media/coding2.24c552c9.svg"
+  }
+]);
